1eec5446fb1540cef10266a93861e053579eccbf
Britt Carlton <brittanyrc95@gmail.com>
2022-07-21 16:23:33 -0400

    Create Manager obj tests, constructor, methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,33 +1,33 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+    const employee = new Employee('Brittany', '234', 'myemail@gmail.com');
 
     expect(employee.name).toBe('Brittany');
     expect(employee.id).toBe('234');
-    expect(employee.email).toBe('brittanyrc95@gmail.com');
+    expect(employee.email).toBe('myemail@gmail.com');
 });
 
 test("gets employee's name", () => {
-    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+    const employee = new Employee('Brittany', '234', 'myemail@gmail.com');
 
     expect(employee.getName()).toBe('Brittany');
 });
 
 test("gets employee's id", () => {
-    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+    const employee = new Employee('Brittany', '234', 'myemail@gmail.com');
 
     expect(employee.getId()).toBe('234');
 });
 
 test("gets employee's email", () => {
-    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+    const employee = new Employee('Brittany', '234', 'myemail@gmail.com');
 
-    expect(employee.getEmail()).toBe('brittanyrc95@gmail.com');
+    expect(employee.getEmail()).toBe('myemail@gmail.com');
 });
 
 test("gets employee's role", () => {
-    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+    const employee = new Employee('Brittany', '234', 'myemail@gmail.com');
 
     expect(employee.getRole()).toHaveProperty('name');
     expect(employee.getRole()).toHaveProperty('id');
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,43 @@
+const Manager = require('../lib/Manager');
+
+test('create a manager object', () => {
+    const data = {
+        name: 'Brittany',
+        id: '234',
+        email: 'myemail@gmail.com',
+        officeNumber: '6'
+    };
+    const manager = new Manager(data);
+
+    expect(manager.name).toBe('Brittany');
+    expect(manager.id).toBe('234');
+    expect(manager.email).toBe('myemail@gmail.com');
+    expect(manager.officeNumber).toBe('6');
+});
+
+test("gets manager's office number", () => {
+    const data = {
+        name: 'Brittany',
+        id: '234',
+        email: 'myemail@gmail.com',
+        officeNumber: '6'
+    };
+    const manager = new Manager(data);
+
+    expect(manager.getOfficeNumber()).toBe('6');
+});
+
+test("gets manager's details", () => {
+    const data = {
+        name: 'Brittany',
+        id: '234',
+        email: 'myemail@gmail.com',
+        officeNumber: '6'
+    };
+    const manager = new Manager(data);
+
+    expect(manager.getRole()).toHaveProperty('name');
+    expect(manager.getRole()).toHaveProperty('id');
+    expect(manager.getRole()).toHaveProperty('email');
+    expect(manager.getRole()).toHaveProperty('officeNumber');
+});
