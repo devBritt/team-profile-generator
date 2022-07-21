7da4ddef0ddcdce5ec41cb4b9e161b4bacf90360
Britt Carlton <brittanyrc95@gmail.com>
2022-07-21 15:52:43 -0400

    Create Employee object tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+
+    expect(employee.name).toBe('Brittany');
+    expect(employee.id).toBe('234');
+    expect(employee.email).toBe('brittanyrc95@gmail.com');
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+
+    expect(employee.getName()).toBe('Brittany');
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+
+    expect(employee.getId()).toBe('234');
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+
+    expect(employee.getEmail()).toBe('brittanyrc95@gmail.com');
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('Brittany', '234', 'brittanyrc95@gmail.com');
+
+    expect(employee.getRole()).toHaveProperty('name');
+    expect(employee.getRole()).toHaveProperty('id');
+    expect(employee.getRole()).toHaveProperty('email');
+});
